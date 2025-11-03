d82fec0c052898ce143762b71bda87690df0452d
Oleksii <kespa71for@gmail.com>
2025-11-03 15:19:58 +0100

    feat: setup navigation routes and fix static paths

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 import express from "express";
 import nunjucks from "nunjucks";
-import cors from "cors"
-
+import cors from "cors";
+import path from "path";
 import type { Request, Response } from "express";
 
 const app = express();
 const PORT = 4000;
 
-app.use(cors())
+const publicPath = path.join(__dirname, '..', 'public'); 
+const viewsPath = path.join(__dirname, '..', 'src', 'view');
 
-nunjucks.configure('src/view', {
+app.use(cors());
+app.use(express.static(publicPath));
+
+nunjucks.configure(viewsPath, {
   autoescape: true,
   express: app,
-  watch: true
+  watch: true,
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.render("index.html", {
-    test: "Test "
-  })
+  res.render("index.html", { test: "Test" });
 });
+
+app.get("/about", (req: Request, res: Response) => {
+  res.render("about.html")
+})
+
+app.get("/contact", (req: Request, res: Response) => {
+  res.render("contact.html")
+})
+
+app.get("/post", (req: Request, res: Response) => {
+  res.render("post.html")
+})
+
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
